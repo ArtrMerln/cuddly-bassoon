a6600c71d83499e687619e4da0e804f521c90d26
3049255 <3049255@MC266459.ifrn.local>
2018-09-24 08:10:10 -0300

    primeiro commit de artur I

---
+++ ArduinoArtur/src/Rapadura.java
@@ -0,0 +1,9 @@
+
+public class Rapadura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("aqui quem fez primeiro foi artur merlin e só");
+	}
+
+}
