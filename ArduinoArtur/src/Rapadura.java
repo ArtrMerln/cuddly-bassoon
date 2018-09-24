1126b1b7345effb76aa3be30039331237cf33581
20151074010042 <20151074010042@MC266464.ifrn.local>
2018-09-24 08:18:34 -0300

    foi vitoria segundo

--- ArduinoArtur/src/Rapadura.java
+++ ArduinoArtur/src/Rapadura.java
@@ -4,6 +4,7 @@ public class Rapadura {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("aqui quem fez primeiro foi artur merlin e só");
+System.out.println("aqui quem fez foi vitoria segundo");
 	}
 
 }
